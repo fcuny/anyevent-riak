f6ac9dc69c1585ce5b3fd9ffd73aa049237c5ea1
franck cuny <franck@lumberjaph.net>
2010-05-11 11:22:58 +0200

    switch to dzil; rename tests; update changelog

--- lib/AnyEvent/Riak/Role/CVCB.pm
+++ lib/AnyEvent/Riak/Role/CVCB.pm
@@ -1,27 +1,19 @@
 package AnyEvent::Riak::Role::CVCB;
 
+# ABSTRACT: return a default condvar and callback if none defined
+
 use Moose::Role;
 
-sub default_cb {
-    my ($self, $options) = @_;
-    return sub {
-        my $res = shift;
-        return $res;
-    };
-}
-
-sub cvcb {
+sub _cvcb {
     my ($self, $options) = @_;
 
-    my ($cv, $cb);
-    $cv = AE::cv;
-    if ($options->{callback}) {
-        $cb = delete $options->{callback};
-    }
-    else {
-        $cb = $self->default_cb();
+    my ($cv, $cb) = (AnyEvent->condvar, sub { return @_ });
+    if ($options && @$options) {
+        $cv = pop @$options if UNIVERSAL::isa($options->[-1], 'AnyEvent::CondVar');
+        $cb = pop @$options if ref $options->[-1] eq 'CODE';
     }
     ($cv, $cb);
 }
 
 1;
+
